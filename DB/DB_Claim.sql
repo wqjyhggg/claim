7ca62f965023a4c269ce0ae3d4b4a75afff636bc
bhawani.shankar <bhawani.shankar@brsoftech.org>
2016-12-01 18:49:09 +0530

    DB Update

--- DB/DB_Claim.sql
+++ DB/DB_Claim.sql
@@ -7,17 +7,34 @@
 CREATE DATABASE IF NOT EXISTS `jf_claim_management` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `jf_claim_management`;
 
+CREATE TABLE IF NOT EXISTS `country` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*!40000 ALTER TABLE `country` DISABLE KEYS */;
+INSERT INTO `country` (`id`, `name`) VALUES
+	(1, 'Canada'),
+	(2, 'United States');
+/*!40000 ALTER TABLE `country` ENABLE KEYS */;
+
 CREATE TABLE IF NOT EXISTS `groups` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `description` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 /*!40000 ALTER TABLE `groups` DISABLE KEYS */;
 INSERT INTO `groups` (`id`, `name`, `description`) VALUES
 	(1, 'admin', 'Administrator'),
-	(2, 'eac', 'EAC Manager');
+	(2, 'eacmanager', 'EAC Manager'),
+	(3, 'callcenteragent', 'Call Centre Agent'),
+	(4, 'casemamager', 'Case Manager'),
+	(5, 'caseexaminer', 'Claims Examiner'),
+	(6, 'claimsmanager', 'Claims Manager'),
+	(7, 'accountant', 'Accountant');
 /*!40000 ALTER TABLE `groups` ENABLE KEYS */;
 
 CREATE TABLE IF NOT EXISTS `login_attempts` (
@@ -30,6 +47,47 @@ CREATE TABLE IF NOT EXISTS `login_attempts` (
 
 /*!40000 ALTER TABLE `login_attempts` DISABLE KEYS */;
 /*!40000 ALTER TABLE `login_attempts` ENABLE KEYS */;
+
+CREATE TABLE IF NOT EXISTS `provider` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `address` text,
+  `postcode` int(8) DEFAULT NULL,
+  `discount` int(10) DEFAULT NULL,
+  `contact_person` varchar(20) DEFAULT NULL,
+  `phone_no` varchar(20) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `ppo_codes` varchar(20) DEFAULT NULL,
+  `services` varchar(200) DEFAULT NULL,
+  `lat` varchar(20) DEFAULT NULL,
+  `lng` varchar(20) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+/*!40000 ALTER TABLE `provider` DISABLE KEYS */;
+INSERT INTO `provider` (`id`, `name`, `address`, `postcode`, `discount`, `contact_person`, `phone_no`, `email`, `ppo_codes`, `services`, `lat`, `lng`, `created`) VALUES
+	(7, 'bhawan', 'gangotri garden, jaipur', 302015, 10, '21321', '32132', 'g8bhawani@gmail.com', '1', '321', '26.8726731', '75.7788451', NULL),
+	(8, 'bhawan', 'sodala, jaipur', 302011, 10, '21321', '32132', 'g8bhawani@gmail.com', '1', '321', '26.9064744', '75.7728014', NULL);
+/*!40000 ALTER TABLE `provider` ENABLE KEYS */;
+
+CREATE TABLE IF NOT EXISTS `province` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `country_id` int(10) NOT NULL DEFAULT '0',
+  `name` varchar(16) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_province_country` (`country_id`),
+  CONSTRAINT `FK_province_country` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*!40000 ALTER TABLE `province` DISABLE KEYS */;
+INSERT INTO `province` (`id`, `country_id`, `name`) VALUES
+	(1, 2, 'Alberta'),
+	(2, 2, 'British Columbia'),
+	(3, 1, 'British Columbia'),
+	(4, 1, 'British Columbia'),
+	(5, 1, 'British Columbia');
+/*!40000 ALTER TABLE `province` ENABLE KEYS */;
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -50,12 +108,13 @@ CREATE TABLE IF NOT EXISTS `users` (
   `company` varchar(100) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`) VALUES
-	(1, '127.0.0.1', 'administrator', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'admin@admin.com', '', NULL, NULL, NULL, 1268889823, 1479899192, 1, 'Admin', 'istrator', 'ADMIN', '0'),
-	(2, '192.168.1.29', 'a@xx.com', '$2y$08$Eh/U0WQXeWIlA3Mmy9gbHevFCzecwlJRmjsE/U./mwrHub.2GEZT.', NULL, 'a@xx.com', NULL, NULL, NULL, NULL, 1479881420, 1479881447, 1, 'nn', 'bb', 'nn', '123131');
+	(1, '127.0.0.1', 'administrator', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'admin@admin.com', NULL, NULL, NULL, NULL, 1268889823, 1480590505, 1, 'Admin', 'istrator', NULL, '2132132132'),
+	(2, '192.168.1.29', 'a@xx.com', '$2y$08$gnsbXPmHtU7SBQko94uf9.VVVzzFhd12fYK3n1FMx4lL8yDPzMvvm', NULL, 'a@xx.com', NULL, NULL, NULL, NULL, 1479881420, 1480055689, 1, 'nn123', 'bb123', NULL, '123131'),
+	(5, '192.168.1.29', 'paytm123e@gmail.com', '$2y$08$p84W1BzwM7WslS9PgioW5elSODLU0E0N/p8Q2uyNzOFeHxD48AW3q', NULL, 'paytm123e@gmail.com', NULL, NULL, NULL, NULL, 1479977418, NULL, 1, 'bhawani', 'bb', NULL, '231321322');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 CREATE TABLE IF NOT EXISTS `users_groups` (
@@ -68,13 +127,19 @@ CREATE TABLE IF NOT EXISTS `users_groups` (
   KEY `fk_users_groups_groups1_idx` (`group_id`),
   CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8;
 
 /*!40000 ALTER TABLE `users_groups` DISABLE KEYS */;
 INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
-	(1, 1, 1),
-	(2, 1, 2),
-	(3, 2, 2);
+	(51, 1, 1),
+	(52, 1, 2),
+	(53, 1, 3),
+	(54, 1, 4),
+	(55, 1, 5),
+	(56, 1, 6),
+	(57, 1, 7),
+	(58, 2, 7),
+	(50, 5, 2);
 /*!40000 ALTER TABLE `users_groups` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
