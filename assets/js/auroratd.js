be42e8d455ec4be2a5d291a0c539b12cc93f42af
Jack1232 <wqjyhggg@gmail.com>
2018-02-27 10:09:32 -0500

    fix

--- assets/js/auroratd.js
+++ assets/js/auroratd.js
@@ -145,6 +145,7 @@ $(document).ready(function(){
 				$('#phone_logout').show();
 				$('#phone_queue_div').show();
 				$('#phone_login').hide();
+				ws_init();
 			} else if (data.login == 'Unknown') {
 				$('#phone_opt_div').hide();
 				$('#phone_queue_div').hide();
@@ -156,4 +157,7 @@ $(document).ready(function(){
 		}
 	})
 	
+	$(window).on('beforeunload', function(){
+		ws_quit();
+	});
 })
