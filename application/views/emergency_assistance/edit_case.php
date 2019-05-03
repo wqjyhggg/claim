7cef3b51e2bca0244bfe2aebf97024cd32e848cc
Jack1232 <wqjyhggg@gmail.com>
2019-05-03 10:10:49 -0400

    fix as required

--- application/views/emergency_assistance/edit_case.php
+++ application/views/emergency_assistance/edit_case.php
@@ -507,7 +507,9 @@
 							</div>
 							<?php } else { ?>
 							<div class="col-sm-10">
-								<div class="col-sm-12"><pre><?php echo $value['notes'] ?></pre></div>
+								<div class="col-sm-12">
+									<?php echo str_replace(array("\r\n", "\n\r", "\n", "\r"), "<br />", $value['notes']); ?>
+								</div>
 								<div class="form-group col-sm-11 files">
 									<br />
 									<?php $files = $value['docs'] ? explode(",", $value['docs']) : array(); ?>
