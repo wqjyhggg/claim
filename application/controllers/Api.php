e53c60b588ff6ef101ad340aca5054092cf009c3
wqjyhggg <wqjyhggg@gmail.com>
2026-09-06 16:08:27 -0400

    Add block list to api

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -961,6 +961,37 @@ class Api extends CI_Controller {
 		echo json_encode($rdata);
 	}
 
+	public function block_check() {
+		$this->load->model('api_model');
+		$ips = array('127.0.0.1', '54.89.143.155', '52.205.81.107', '54.164.58.203');
+		$keys = array('qqnzcPfp', 'H5FqpJdc');
+		
+		$key = $this->input->get_post('key');
+		$rdata = array('status' => Api_model::STATUS_ERROR, 'message' => 'Unknown Error');
+		if (! in_array($_SERVER['REMOTE_ADDR'], $ips) || ! in_array($key, $keys)) {
+			die(json_encode($rdata));
+		}
+		
+		$this->load->model('expenses_model');
+		$this->load->model('case_model');
+		$this->load->model('block_list_model');
+		
+		$firstname = strtolower($this->input->get_post('firstname'));
+		$lastname = strtolower($this->input->get_post('lastname'));
+		$birthday = $this->input->get_post('birth');
+		
+		$rt = $this->block_list_model->check_list_name($firstname, $lastname, $birthday);
+		$rdata['isblocked'] = (empty($rt) || ($rt["status"] != 2)) ? 0 : 1;
+		$rdata['status'] = Api_model::STATUS_OK;
+		$rdata['message'] = '';
+		
+		header('Content-Type: application/json');
+		header('Access-Control-Allow-Origin: *');
+		header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+		header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+		echo json_encode($rdata);
+	}
+
 	public function claim_search() {
 		$this->load->model('api_model');
 		$ips = array('127.0.0.1', '54.89.143.155', '52.205.81.107', '54.164.58.203');
