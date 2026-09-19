2d04120cd7b7522616434616b670f7692c0cf04b
wqjyhggg <wqjyhggg@gmail.com>
2026-09-19 10:32:41 -0400

    More fix

--- application/models/Reasons_model.php
+++ application/models/Reasons_model.php
@@ -15,8 +15,7 @@ class Reasons_model extends CI_Model {
 	 * @return array result array, maybe null
 	 */
 	public function get_list() {
-		$this->db->order_by('name');
-		$rt = $this->db->order_by('id', 'ASC')->get('reasons')->result_array();
+		$rt = $this->db->order_by('isfrench', 'ASC')->order_by('name')->get('reasons')->result_array();
 		$rArr = array();
 		foreach ($rt as $rc) {
 			$rArr[$rc['id']] = $rc;
