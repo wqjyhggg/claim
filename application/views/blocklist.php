0b7e99af60a533b45806cb8392d2aed39f3be2e6
wqjyhggg <wqjyhggg@gmail.com>
2026-09-23 19:29:49 -0400

    More

--- application/views/blocklist.php
+++ application/views/blocklist.php
@@ -141,7 +141,7 @@
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
-          <h4 class="modal-title" id="notesModalLabel"><span id="tFirstname"></span> <span id="tLastname"></span> <span id="tBirthday"></span></h4>
+          <h4 class="modal-title" id="notesModalLabel"><span id="tFirstname"></span> <span id="tLastname"></span> (<span id="tBirthday"></span>)</h4>
         </div>
         <div class="modal-body">
           <!-- Current Notes -->
